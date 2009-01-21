cba21a6eb570dcb403d7992baef70c3e8be7f98a
James Cox <james@imaj.es>
2009-01-21 02:39:01 +0000

    remove rails.png

--- rails_template.rb
+++ rails_template.rb
@@ -9,6 +9,7 @@ run "rm README"
 run "rm public/index.html"
 run "rm public/favicon.ico"
 run "rm public/robots.txt"
+run "rm public/images/rails.png"
 run "rm -f public/javascripts/*"
 
 # Download JQuery
