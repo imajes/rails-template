aeb6a861306335d27800ab3c0cb6af1ce33cf0f0
James Cox <james@imaj.es>
2009-01-21 01:05:53 +0000

    complete template

--- rails_template.rb
+++ rails_template.rb
@@ -2,36 +2,42 @@
 # SUPER DARING APP TEMPLATE 1.0 - By Peter Cooper
 
 # Link to local copy of edge rails
-  inside('vendor') { run 'ln -s ~/src/git/rails rails' }
+inside('vendor') { run 'ln -s ~/src/git/rails rails' }
 
 # Delete unnecessary files
-  run "rm README"
-  run "rm public/index.html"
-  run "rm public/favicon.ico"
-  run "rm public/robots.txt"
-  run "rm -f public/javascripts/*"
+run "rm README"
+run "rm public/index.html"
+run "rm public/favicon.ico"
+run "rm public/robots.txt"
+run "rm -f public/javascripts/*"
 
 # Download JQuery
-  run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js > public/javascripts/jquery.js"
-  run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
+run "curl -L http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js > public/javascripts/jquery.js"
+run "curl -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
 
 # Set up git repository
-  git :init
-  git :add => '.'
-  
+git :init
+git :add => '.'
+
 # Copy database.yml for distribution use
-  run "cp config/database.yml config/database.yml.example"
-  
+run "cp config/database.yml config/database.yml.example"
+
 # Set up .gitignore files
-  run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
-  run %{find . -type d -empty | grep -v "vendor" | grep -v ".git" | grep -v "tmp" | xargs -I xxx touch xxx/.gitignore}
-  file '.gitignore', <<-END
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+run %{find . -type d -empty | grep -v "vendor" | grep -v ".git" | grep -v "tmp" | xargs -I xxx touch xxx/.gitignore}
+file '.gitignore', <<-END
 .DS_Store
 log/*.log
 tmp/**/*
 config/database.yml
 db/*.sqlite3
 END
+
+# Set up session store initializer
+initializer 'session_store.rb', <<-END
+ActionController::Base.session = { :session_key => '_#{(1..6).map { |x| (65 + rand(26)).chr }.join}_session', :secret => '#{(1..40).map { |x| (65 + rand(26)).chr }.join}' }
+ActionController::Base.session_store = :active_record_store
+  END
 
 # Install submoduled plugins
 ## Those that relate to testing
@@ -40,17 +46,21 @@ plugin 'rspec-rails', :git => 'git://github.com/dchelimsky/rspec-rails.git', :su
 plugin 'machinist', :git => 'git://github.com/notahat/machinist.git', :submodule => true
 generate("rspec")
 gem 'faker'
-## setup for the win
-inside ('spec') { run "mkdir blueprints" }
-inside ('spec') { run "rm -rf fixtures" }
 
+## setup for the win
+inside ('spec') { 
+  run "mkdir blueprints" 
+  run "rm -rf fixtures"
+  run "rm spec_helper.rb"
+  run "wget http://github.com/imajes/rails-template/raw/master/spec_helper.rb"
+}
 
 ## Potentially Useful 
 plugin 'asset_packager', :git => 'git://github.com/sbecker/asset_packager.git', :submodule => true
 plugin 'exception_notifier', :git => 'git://github.com/rails/exception_notification.git', :submodule => true
 
 ## user related
-if ask("Will this app have authenticated users?")
+if yes?("Will this app have authenticated users?")
   plugin 'open_id_authentication', :git => 'git://github.com/rails/open_id_authentication.git', :submodule => true
   plugin 'role_requirement', :git => 'git://github.com/timcharper/role_requirement.git', :submodule => true
   plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git', :submodule => true
@@ -62,13 +72,13 @@ if ask("Will this app have authenticated users?")
 end
 
 # tags
-if ask("Do you want tags with that?")
+if yes?("Do you want tags with that?")
   plugin 'acts_as_taggable_redux', :git => 'git://github.com/monki/acts_as_taggable_redux.git', :submodule => true
   rake('acts_as_taggable:db:create')
 end
 
 # require some gems
-if ask("Want to require a bunch of useful gems?")
+if yes?("Want to require a bunch of useful gems?")
   gem 'hpricot', :source => 'http://code.whytheluckystiff.net'
   gem 'RedCloth', :lib => 'redcloth'
 end
@@ -78,18 +88,15 @@ rake('gems:install', :sudo => true)
 rake('db:sessions:create')
 rake('db:migrate')
 
-# Set up session store initializer
-  initializer 'session_store.rb', <<-END
-ActionController::Base.session = { :session_key => '_#{(1..6).map { |x| (65 + rand(26)).chr }.join}_session', :secret => '#{(1..40).map { |x| (65 + rand(26)).chr }.join}' }
-ActionController::Base.session_store = :active_record_store
-  END
+first = ask("What'll be your first action?")
+generate(:model, first)
  
 # Initialize submodules
-  git :submodule => "init"
+git :submodule => "init"
 
 # Commit all work so far to the repository
-  git :add => '.'
-  git :commit => "-a -m 'Initial commit'"
+git :add => '.'
+git :commit => "-a -m 'Initial commit'"
 
 # Success!
-  puts "SUCCESS!"
+puts "SUCCESS!"
